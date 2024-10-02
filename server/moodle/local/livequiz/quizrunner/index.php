9696fa90961517925dfc29e0eacb3752074726e6
Vincent Kosteyev Bechmann <vbechm22@student.aau.dk>
2024-10-02 14:27:48 +0200

    Wrote some docs

--- server/moodle/local/livequiz/quizrunner/index.php
+++ server/moodle/local/livequiz/quizrunner/index.php
@@ -1,4 +1,31 @@
 <?php
+
+/**
+ * EXPLANATION STUFF FOR HOW ALL THIS IS SET UP
+ * ============================================
+ *
+ * We've made a object-oriented representation of the quiz runner. 
+ * 
+ * At the top is an interface `answer` (`answer/answer.php`), which 
+ * only has the method `html()`, which makes the answer form. We 
+ * expect it to contain a submit button that goes to `wait.php`. 
+ *
+ * We're not quite sure how we want to structure the overall flow of 
+ * the program, so that's something we need to figure out.
+ *
+ * Then we have the more specific classes, so `slider` 
+ * (`answer/slider.php`), and `multichoice` (`answer/multichoice.php`), 
+ * whose file also contains the `multichoice_choice` class. For an 
+ * example on how to initialise there, see the code in this file.
+ *
+ * A couldhave to-do is to make an image labelling answer class.
+ *
+ * Best regards,
+ * Team Lennart.
+ *
+ */
+
+
 require_once('../../../config.php');
 
 require_once('question.php');
@@ -15,14 +42,14 @@ echo $OUTPUT->header();
 $question = new question();
 $question->image = 'fish.png';
 $question->prompt = 'Is fish fishing????';
-/*$question->answer = new multichoice(
+$question->answer = new multichoice(
 	array(
 		new multichoice_choice("Yes!!!!", "yes"),
 		new multichoice_choice("No!!!!!", "no"),
-		new multichoice_choice("Mayhaps...", "perchance")
+		new multichoice_choice("Mayhaps...", "maybe")
 	)
-);*/
-$question->answer = new slider(0, 10);
+);
+//$question->answer = new slider(0, 10);
 
 $question->display();
 

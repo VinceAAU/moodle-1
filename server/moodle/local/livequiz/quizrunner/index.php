ecba0a6b9c89318c2ad89898c3de8aedfb0a0336
Vincent Kosteyev Bechmann <vbechm22@student.aau.dk>
2024-10-02 11:39:04 +0200

    Separated index.php into several files
    Co-authored-by: Theis194 <noreply@github.com>

--- server/moodle/local/livequiz/quizrunner/index.php
+++ server/moodle/local/livequiz/quizrunner/index.php
@@ -1,101 +1,28 @@
 <?php
 require_once('../../../config.php');
+
+require_once('question.php');
+require_once('answer/slider.php');
+
+
 $PAGE->set_url(new moodle_url('/local/livequiz/quizrunner'));
 $PAGE->set_context(context_system::instance());
 $PAGE->set_title("Play quiz");
 $PAGE->set_heading("Join a quiz");
 
-//$PAGE->requires->js_call_amd('local_quiz/playquiz', 'init');
-
 echo $OUTPUT->header();
-
-//Must have a submit button, obviously
-interface answer {
-	public function html() : string;
-}
-
-class multichoice_answer_possibility {
-	public string $display;
-	public string $value;
-
-	public function __construct(string $display, string $value){
-		$this->display	= $display;
-		$this->value	= $value;
-	}
-}
-
-class multichoice_answer implements answer { 
-	private array $choices;
-
-	public function __construct(array $choices){
-		$this->choices = $choices;
-	}
-
-	public function html() : string { 
-		$output = '<form action="quizrunner/wait.php" method="POST" class="answer multichoice">';
-
-		foreach ($this->choices as $choice){
-			$output .= "<button type='submit' name='answer' value='$choice->value'>$choice->display</button>";
-		}
-
-		$output .= '</form>';
-
-		return $output;
-	}
-}
-
-class slider_answer implements answer {
-	private $min;
-	private $max;
-
-	public function __construct(int $min, int $max){
-		$this->min = $min;
-		$this->max = $max;
-	}
-
-	public function html() : string {
-		return "
-			<form action='quizrunner/wait.php' method='POST' class='answer slider'>
-				<output>24</output><br>
-				<input type='range' name='answer' min='$this->min' max='$this->max' oninput='this.previousElementSibling.previousElementSibling.value = this.value'>
-				<button type='submit' name='submit''>Submit</button>
-			</form>
-		";
-	}
-}
-
-class question {
-	public string $image;
-	public string $prompt;
-	public answer $answer;
-	
-	public function html(){
-		return "
-<div>
-	<img src='$this->image'/>
-	<div class='progress-bar'>??%</div>
-	<div class='prompt'>
-		<p>$this->prompt</p>
-	</div>
-	{$this->answer->html()}
-		";
-	}
-
-	public function display(){
-		echo $this->html();
-	}
-}
 
 $question = new question();
 $question->image = 'fish.png';
 $question->prompt = 'Is fish fishing????';
-/* $question->answer = new multichoice_answer(
+/*$question->answer = new multichoice(
 	array(
-		new multichoice_answer_possibility("Yes!!!!", "yes"),
-		new multichoice_answer_possibility("No!!!!!", "no")
+		new multichoice_choice("Yes!!!!", "yes"),
+		new multichoice_choice("No!!!!!", "no"),
+		new multichoice_choice("Mayhaps...", "perchance")
 	)
-); */
-$question->answer = new slider_answer(0, 10);
+);*/
+$question->answer = new slider(0, 10);
 
 $question->display();
 

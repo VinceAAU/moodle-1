d4b8792ac104c3627526c0c7fbfc79ce768a3566
Vincent <vbechm22@student.aau.dk>
2024-10-14 14:56:57 +0200

    Add files via upload

---
+++ .github/workflows/install_script.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+rm -rf vince-test
+git clone https://github.com/VinceAAU/P5 vince-test
+
+set -x
+
+REPO_ROOT="`realpath "vince-test/"`"
+PART_2_SCRIPT="$(realpath $(dirname "$0"))/install_script_part_2.sh"
+
+sed -i \
+	-e 's/;extension=gd/extension=gd/' \
+	-e 's/;extension=intl/extension=intl/' \
+	-e 's/;extension=sodium/extension=sodium/' \
+	-e 's/;extension=zip/extension=zip/' \
+	-e "s|curl\.cainfo.*|curl.cainfo=\"$REPO_ROOT/cacert.pem\"|" \
+	-e "s|openssl\.cafile.*|openssl.cafile=\"$REPO_ROOT\"/server/apache/bin/curl-ca-bundle.crt|" \
+	"$REPO_ROOT"/server/php/php.ini
+
+cd "$REPO_ROOT"
+
+curl https://curl.se/ca/cacert.pem >"$REPO_ROOT"/cacert.pem
+
+nix-shell --run "REPO_ROOT=$REPO_ROOT sh $PART_2_SCRIPT"
+
+echo "DID STUFF:)"
---
+++ .github/workflows/install_script_part_2.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cd "$REPO_ROOT"/server/moodle
+composer require --dev phpunit/phpunit
+mkdir "$REPO_ROOT"/server/moodledata/phpunit
+
+php admin/cli/install.php \
+	--lang=en \
+	--wwwroot="http://localhost:8000/" \
+	--dataroot="$REPO_ROOT/server/moodledata" \
+	--dbpass=root \
+	--dbport=3306 \
+	--dbsocket=/tmp/mysqld.sock \
+	--skip-database \
+	--non-interactive \
+	--agree-license \
+	--allow-unstable \
+	--fullname="Moodle testing thing" \
+	--shortname="mtt" \
+	--adminpass="hunter2"
+
+echo "\$CFG->phpunit_prefix = 'phpu_';" >>"$REPO_ROOT"/server/moodle/config.php
+# Specifying that it's the realpath isn't strictly necessary (because 
+# we make it realpath on line ~3, but we do this redundantly anyway in 
+# case some klaphat decides to modify line 3)
+echo "\$CFG->phpunit_dataroot = '$(realpath "$REPO_ROOT"/server/moodledata/phpunit)';">>"$REPO_ROOT"/server/moodle/config.php
+
+php admin/tool/phpunit/cli/init.php
+
+./vendor/bin/phpunit

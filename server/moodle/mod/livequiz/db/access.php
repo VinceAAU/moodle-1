ea64129b58e33ad62656d523b4e3e4b457e368cd
Theis Jensen <Theis@bmpnj.dk>
2024-11-25 14:17:29 +0100

    Figured out how to get right context
    Co-authored-by: MShadiF <MShadiF@users.noreply.github.com>

--- server/moodle/mod/livequiz/db/access.php
+++ server/moodle/mod/livequiz/db/access.php
@@ -35,6 +35,7 @@ $capabilities = [
         'contextlevel' => CONTEXT_MODULE,
         'archetypes' => [
             'guest' => CAP_ALLOW,
+            'user' => CAP_ALLOW,
             'student' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
